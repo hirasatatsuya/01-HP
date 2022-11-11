83fb32dff369728428ee3898eb1d76447d889ad4
takeru <al20025@sic.shibaura-it.ac.jp>
2022-11-11 18:56:32 +0900

    たけるの初回コミット

--- page-team01.php
+++ page-team01.php
@@ -1,2 +1,179 @@
 <!-- 下層1ページ目 01会 -->
 <!-- たける -->
+<?php
+get_header();
+?>
+  <main id="page-01">
+    <div class="bg">
+      <div class="triangle1"></div>
+      <div class="bar1"></div>
+      <div class="bar2 fadeTrigger"></div>
+      <div class="triangle2 fadeTrigger"></div>
+      <div class="triangle3 fadeTrigger"></div>
+    </div>
+    <section id="page_mv">
+      <h2 class="title">team01</h2>
+      <h2 class="title_ja">01会</h2>
+    </section>
+
+
+    <section id="a">
+
+
+  </section>
+    
+    <section id="thought">
+      <div class="background-img slideUpTrigger">
+        <!-- <div class="arrow"> -->
+          <!-- <h2>01会の思い</h2> -->
+        <!-- </div>  -->
+        <div class="wrapper">
+          <div class="tech-slideshow">
+            <div class="mover-1"></div>
+            <div class="mover-2"></div>
+          </div>
+          <div class="thought-text slideLeftTrigger">
+            <h2>01会の思い</h2>
+            <h3>team01</h3>
+            <p>
+              2021年10月より立ち上がった「01会」。2001年生まれ世代限定のコミュニティです。これがなければ株式会社ゼロイチはありませんでした。最高の仲間と出会える場所、それが「01会」です。これからは株式会社ゼロイチの一つの事業として「01会」を全国に展開し、より発展させていきます。ここに集まってくれた仲間のために最高に『可笑しい』コミュニティを運営します。
+            </p>
+            <div class="btn-01">
+              <a href="#" class="btn_04">01会へのお問い合わせ</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="event" class="slideUpTrigger">
+      <div class="event-title">
+        <h2 class="heading10 slideLeftTrigger" data-en="Event"><span>01会のイベント実績</span></h2>
+      </div>
+        
+      <span class="event_btn prev fa fa-arrow-circle-left"></span>
+      <span class="event_btn next fa fa-arrow-circle-right"></span>
+      
+    <section id="abc">
+      <div class="half pictures">
+        <div class="picture-container picture-1 displayed"></div>
+        <div class="picture-container picture-2"></div>
+        <div class="picture-container picture-3"></div>
+        <div class="picture-container picture-4"></div>
+        <div class="picture-container picture-5"></div>
+      </div>
+        <div class="half descriptions">
+          <div class="description-container description-1 displayed">
+            <h2 class="heading06" data-en="Kyoto">in京都</h2>
+            <p>初めての「01会」は京都で開催しました。ここで出会えた仲間から「01会」のストーリーは始まりました。</p>
+          </div>
+          <div class="description-container description-2">
+            <h2 class="heading06" data-en="Yokohama">in神奈川（横浜）</h2>
+            <p>関東運営として初めて開催しました。台風の影響もあり、少ない人数でしたがみんなでクイズ大会をして交流しました。</p>
+          </div>
+          <div class="description-container description-3">
+            <h2 class="heading06" data-en="Fukuoka">in福岡</h2>
+            <p>ついに九州上陸。九州はなんといってもメンバーの個性が豊かすぎました。各自メンバーが面白すぎてついつい時間が押してしまいました。</p>
+          </div>
+          <div class="description-container description-4">
+            <h2 class="heading06" data-en="Kyoto">in京都<br>（舞鶴合宿）</h2>
+            <p>「01会」としてはじめての合宿になりました。たくさんの大人の方々にもご協力いただき、また企業、行政との連携も図りながら夏の最高の思い出をつくりました。</p>
+          </div>
+          <div class="description-container description-5">
+            <h2 class="heading06" data-en="Osaka">in大阪</h2>
+            <p>今回は大阪にてキャリアについて考えるバーベキューを開催しました。楽天の社員さんやキャリアコーディネーターなど様々な方に来て頂き、自分のキャリアについて考える会になりました。</p>
+          </div>
+      </div>
+    </section>
+  </section>
+
+
+  <section id="member">
+    <h2 class="heading10 slideLeftTrigger" data-en="Member"><span>01会のメンバー</span></h2>
+    <div class="wrapper slideUpTrigger">
+      <div class="wrap slide-paused" ontouchstart="">
+        <ul class="slideshow">
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+          </li>
+
+        </ul>
+
+        <ul class="slideshow">
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+        </ul>
+
+        <ul class="slideshow">
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+          </li>
+          <li class="content content-hover">
+            <img src="img/haruto1.jpg" alt="">
+            <p>あああああ</p>
+        </ul>
+
+      </div>
+
+      <div class="btn-01">
+        <a href="#" class="btn_04">01会のメンバー紹介</a>
+      </div>
+    </div>
+        </div>
+    </div>
+  </section>
+
+
+  <section id="member-achivement">
+    <h2 class="heading10 slideLeftTrigger" data-en="Achivement"><span>01会のメンバー実績</span></h2>
+    <div class="wrapper slideUpTrigger">
+      <div class="member-achivement-text">
+        <h2>COMING SOON</h2>
+        <!-- <img src="img/LINE_ALBUM_ゼロイチロゴ_221012.jpg" alt=""> -->
+      </div>
+      <div class="container">
+
+      </div>
+    </div>
+  </section>
+
+  </main>
+  <?php
+get_footer();
+?>
---
+++ page-zeroichi_members.php
@@ -0,0 +1,255 @@
+<!--下層2ページ目 01会メンバー実績ページ -->
+<!-- 庄司担当 -->
+<?php get_header(); ?>
+    <main id="philosophy">
+        <div class="bg">
+            <div class="triangle1"></div>
+            <div class="bar1"></div>
+            <div class="bar2 fadeTrigger"></div>
+            <div class="triangle2 fadeTrigger"></div>
+            <div class="triangle3 fadeTrigger"></div>
+        </div>
+        <section id="page_mv">
+            <h2 class="title">01MEMBERS</h2>
+            <h2 class="title_ja">メンバー紹介</h2>
+        </section>
+        <section id="members">
+            <div class="wrapper">
+                <div class="member-flex-parent">
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+                                    <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+                                    <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- </div> -->
+                    <!-- <div class="member-flex-parent"> -->
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+                                    <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="img/img-tetsuya01_183513e0-c42e-4f69-be2b-d7f011cd33bc.webp" alt="img">
+                        </div>
+                        <div class="member-content">
+                            <h2>小柳津てつや</h2>
+                            <p>平社員</p>
+                            <div class="member_sns_wrapper">
+                                <ul class="member_sns">
+                                    <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+                                    <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <footer id="footer">
+        <section id="footer_contact">
+            <div class="footer_contact_wrapper">
+                <div class="footer_contact_contents">
+                    <a href="">
+                        <p>
+                            お問合せはこちら　＞
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section id="footer_main">
+            <div class="footer_main_wrapper">
+                <div class="footer_sns_wrapper">
+                    <ul class="footer_sns">
+                        <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
+                        <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
+                        <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
+                        <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
+                    </ul>
+                </div>
+                <nav class="footer_nav">
+                    <div class="col">
+                        <h4><a href="">経営理念</a></h4>
+                        <ul>
+                            <li><a href="">Vision・Mission・Value</a></li>
+                            <li><a href="">01会の思い</a></li>
+                            <li><a href="">01の由来</a></li>
+                        </ul>
+                        <h4><a href="">事業紹介</a></h4>
+                        <h4><a href="">実績紹介</a></h4>
+                        <ul>
+                            <li><a href="">映像</a></li>
+                            <li><a href="">システム開発</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4><a href="">会社概要</a></h4>
+                        <ul>
+                            <li><a href="">代表挨拶<br class="sp_560">・役員紹介</a></li>
+                            <li><a href="">会社情報</a></li>
+                        </ul>
+                        <h4><a href="">お問合せ</a></h4>
+                    </div>
+                    <div class="col">
+                        <h4><a href="">01会</a></h4>
+                        <ul>
+                            <li><a href="">01の思い</a></li>
+                            <li><a href="">イベント実績</a></li>
+                            <li><a href="">メンバー実績</a></li>
+                            <li><a href="">01会のお問合せ</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </section>
+        <section id="footer_copy">
+            <p>&copy;2022 株式会社01</p>
+        </section>
+    </footer>
+</body>
