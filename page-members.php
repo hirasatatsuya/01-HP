c1b6a06ae62e8d334f6249bf74d2454250f941fc
takeru <al20025@sic.shibaura-it.ac.jp>
2022-11-19 00:46:18 +0900

    takeru commit branch

--- page-members.php
+++ page-members.php
@@ -1,2 +1,108 @@
 <!-- 下層2ページ目 代表挨拶・役員紹介-->
 <!-- たける -->
+<?php get_header() ?>
+<main id="page-members">
+    <div class="bg">
+      <div class="triangle1"></div>
+      <div class="bar1"></div>
+      <div class="bar2 fadeTrigger"></div>
+      <div class="triangle2 fadeTrigger"></div>
+      <div class="triangle3 fadeTrigger"></div>
+    </div>
+    <section id="page_mv">
+      <h2 class="title">Executive</h2>
+      <h2 class="title_ja">役員紹介</h2>
+    </section>
+
+    <div class="section-title">  
+      <h2 class="heading10 slideLeftTrigger" data-en="Member"><span>代表挨拶</span></h2>
+    </div>
+    <section id="CEO" class="slideUpTrigger">
+      <div class="wrapper">
+
+        <div class="CEO-info">
+          <div class="CEO-introduce">
+            <!-- <div class="CEO-info-left">
+              <div class="CEO-title-upper">
+                <h2 class="hello-CEO">代表挨拶</h2>
+                <p class="hello-CEO-eng">message</p>
+              </div>
+              <div class="CEO-title-lower">
+              </div> -->
+              <div class="CEO-title-right">
+                <h3 class="CEO-title">
+                  「伝えるを"より"自由に」
+              </h3>
+              <p class="CEO-text">
+                今、少子高齢化により若者は減ってきています。だからこそ、我々若者は危機感を感じています。
+                私たち世代でこの世の中を変えるべく立ち上がった「01会」。ゼロからイチを創るため、ゼロイチ世代というブランドを創るため、日本を底上げするために私は取り組んでまいりました。
+                「伝えるを"より"自由に」という経営理念の下、私たちが提供させていただいているものが、より皆様の身近な存在になるべく、そして皆様の伝える手段を広げるため映像やWEB制作の観点より取り組んでまいります。
+                私たちが、皆様一人ひとりの快適な未来へつながっていく、その強い想いを持ってこれからも日々あらゆる事業に邁進していく所存です。
+                今後とも益々のご支援とご愛好を賜りますようお願い申し上げます。  
+              </p>
+              <p class="company-name">株式会社01</p>
+              <p class="CEO-name">代表取締役 奥田悠友</p>
+            </div>
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </section>
+
+    <section id="executives">
+      <div class="wrapper">
+        <h2 class="heading10 slideLeftTrigger" data-en="Member"><span>役員紹介</span></h2>
+        <!-- <h2 class="executives-title slideUpTrigger">役員紹介</h2>
+        <p class="executives-title-eng slideUpTrigger">EXECUTIVES</p> -->
+
+        <div class="executives-introduce">
+          <ul class="executives-list">
+            <li class="executives-item slideLeftTrigger">
+              <div class="executives-img">
+                <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
+              </div>
+              <div class="executives-info">
+                <h3 class="executives-position">エンジニア</h3>
+                <p class="executives-name">平佐竜也</p>
+                <p class="executives-comment">
+                  テキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキスト
+                </p>
+              </div>
+            </li>
+            <li class="executives-item-2 slideRightTrigger">
+              <div class="executives-img">
+                <img src="<?php echo esc_url(get_theme_file_uri('/img/naoki1.jpg')) ?>" alt="">
+              </div>
+              <div class="executives-info">
+                <h3 class="executives-position">映像</h3>
+                <p class="executives-name">松永直樹</p>
+                <p class="executives-comment">
+                  テキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキスト
+                </p>
+              </div>
+            </li>
+            <li class="executives-item slideLeftTrigger">
+              <div class="executives-img">
+                <img src="<?php echo esc_url(get_theme_file_uri('/img/kanta1.jpg')) ?>" alt="">
+              </div>
+              <div class="executives-info">
+                <h3 class="executives-position">映像</h3>
+                <p class="executives-name">稲場貫太</p>
+                <p class="executives-comment">
+                  テキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキスト
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+
+  </main>
+<?php get_footer() ?>
