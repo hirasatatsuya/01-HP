d946e450f902e81be8d509610f5b7a70bf736c20
takerumaguro <106137505+takerumaguro@users.noreply.github.com>
2022-11-20 20:05:58 +0900

    Takeru dev new (#14)
    * takeru commit branch
    
    * Merge branch 'main' of https://github.com/hirasatatsuya/01-HP into takeru_dev

--- page-information.php
+++ page-information.php
@@ -27,11 +27,11 @@ get_header();
 
 
     <section id="company">
-      <div class="wrapper slideUpTrigger">
+      <div class="wrapper">
       <h2 class="heading10 slideLeftTrigger" data-en="COMPANY"><span>会社概要</span></h2>
-        <dl class="company-list">
-          <dt class="company-title">社名</dt>
-          <dd class="company-info">株式会社01</dd>
+        <dl class="company-list slideUpTrigger">
+          <dt class="company-title u_paragraph">社名</dt>
+          <dd class="company-info u_paragraph">株式会社01</dd>
           <dt class="company-title">設立</dt>
           <dd class="company-info">2022年10月15日</dd>
           <dt class="company-title">代表者</dt>
@@ -60,31 +60,32 @@ get_header();
       <figure class="snip1543">
         <img src="<?php echo esc_url(get_theme_file_uri('/img/fgn2.jpg')) ?>" />
       <figcaption>
-        <h3>LOCATION</h3>
-        <p>福岡県中央区大名2-6-11</p>
+        <h3 class="u_title">LOCATION</h3>
+        <p class="u_paragraph_min">福岡県中央区大名2-6-11</p>
+
       </figcaption>
       <a href="#"></a>
     </figure>
     <figure class="snip1543 hover"><img src="<?php echo esc_url(get_theme_file_uri('/img/fgn-3.jpg')) ?>" />
       <figcaption>
-        <h3>OFFICE</h3>
-        <p>株式会社01のオフィス</p>
+        <h3 class="u_title">OFFICE</h3>
+        <p class="u_paragraph_min">株式会社01のオフィス</p>
       </figcaption>
       <a href="#"></a>
     </figure>
     <figure class="snip1543"><img src="<?php echo esc_url(get_theme_file_uri('/img/fgn.jpg')) ?>" />
       <figcaption>
-        <h3>FGN</h3>
-        <p>FUKUOKA GROWTH NEXT</p>
+        <h3 class="u_title">FGN</h3>
+        <p class="u_paragraph_min">FUKUOKA GROWTH NEXT</p>
       </figcaption>
       <a href="#"></a>
     </figure>
     </section>
 
     <section id="company-location">
-      <div class="wrapper slideUpTrigger">
+      <div class="wrapper">
       <h2 class="heading10 slideLeftTrigger" data-en="ACCESS"><span>アクセス</span></h2>
-        <div class="company-location-flex">
+        <div class="company-location-flex slideUpTrigger">
           <div class="company-location-map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.6220312257356!2d130.3953071!3d33.5891616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x354191860efba4d3%3A0xd33287b5f6832abf!2z44CSODEwLTAwNDEg56aP5bKh55yM56aP5bKh5biC5Lit5aSu5Yy65aSn5ZCN77yS5LiB55uu77yW4oiS77yR77yR!5e0!3m2!1sja!2sjp!4v1665123874862!5m2!1sja!2sjp" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
--- page-members.php
+++ page-members.php
@@ -22,26 +22,19 @@
 
         <div class="CEO-info">
           <div class="CEO-introduce">
-            <!-- <div class="CEO-info-left">
-              <div class="CEO-title-upper">
-                <h2 class="hello-CEO">代表挨拶</h2>
-                <p class="hello-CEO-eng">message</p>
-              </div>
-              <div class="CEO-title-lower">
-              </div> -->
               <div class="CEO-title-right">
-                <h3 class="CEO-title">
+                <h3 class="CEO-title u_title">
                   「伝えるを"より"自由に」
               </h3>
-              <p class="CEO-text">
+              <p class="CEO-text u_paragraph">
                 今、少子高齢化により若者は減ってきています。だからこそ、我々若者は危機感を感じています。
                 私たち世代でこの世の中を変えるべく立ち上がった「01会」。ゼロからイチを創るため、ゼロイチ世代というブランドを創るため、日本を底上げするために私は取り組んでまいりました。
                 「伝えるを"より"自由に」という経営理念の下、私たちが提供させていただいているものが、より皆様の身近な存在になるべく、そして皆様の伝える手段を広げるため映像やWEB制作の観点より取り組んでまいります。
                 私たちが、皆様一人ひとりの快適な未来へつながっていく、その強い想いを持ってこれからも日々あらゆる事業に邁進していく所存です。
                 今後とも益々のご支援とご愛好を賜りますようお願い申し上げます。  
               </p>
-              <p class="company-name">株式会社01</p>
-              <p class="CEO-name">代表取締役 奥田悠友</p>
+              <p class="company-name u_paragraph">株式会社01</p>
+              <p class="CEO-name u_paragraph">代表取締役 奥田悠友</p>
             </div>
             
           </div>
@@ -64,9 +57,9 @@
                 <img src="<?php echo esc_url(get_theme_file_uri('/img/hirasa1.jpg')) ?>" alt="">
               </div>
               <div class="executives-info">
-                <h3 class="executives-position">エンジニア</h3>
-                <p class="executives-name">平佐竜也</p>
-                <p class="executives-comment">
+                <h3 class="executives-position u_title">エンジニア</h3>
+                <p class="executives-name u_subtitle">平佐竜也</p>
+                <p class="executives-comment u_paragpraph">
                   テキストテキストテキストテキストテキストテキスト
                   テキストテキストテキストテキストテキストテキスト
                 </p>
@@ -77,9 +70,9 @@
                 <img src="<?php echo esc_url(get_theme_file_uri('/img/naoki1.jpg')) ?>" alt="">
               </div>
               <div class="executives-info">
-                <h3 class="executives-position">映像</h3>
-                <p class="executives-name">松永直樹</p>
-                <p class="executives-comment">
+                <h3 class="executives-position u_title">映像</h3>
+                <p class="executives-name u_subtitle">松永直樹</p>
+                <p class="executives-comment u_paragraph">
                   テキストテキストテキストテキストテキストテキスト
                   テキストテキストテキストテキストテキストテキスト
                 </p>
@@ -90,9 +83,9 @@
                 <img src="<?php echo esc_url(get_theme_file_uri('/img/kanta1.jpg')) ?>" alt="">
               </div>
               <div class="executives-info">
-                <h3 class="executives-position">映像</h3>
-                <p class="executives-name">稲場貫太</p>
-                <p class="executives-comment">
+                <h3 class="executives-position u_title">映像</h3>
+                <p class="executives-name u_subtitle">稲場貫太</p>
+                <p class="executives-comment u_paragraph">
                   テキストテキストテキストテキストテキストテキスト
                   テキストテキストテキストテキストテキストテキスト
                 </p>
