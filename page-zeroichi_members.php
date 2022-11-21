bda80cddfcc50bcf9bbb7787fb263f9a3f0dbc5b
ShojiAyumu <nyanpas.78963@gmail.com>
2022-11-22 00:29:47 +0900

    イベント実績、メンバー紹介編集＠庄司

--- page-zeroichi_members.php
+++ page-zeroichi_members.php
@@ -1,16 +1,16 @@
 <!--下層2ページ目 01会メンバー実績ページ -->
 <!-- 庄司担当 -->
 <?php get_header(); ?>
-    <main id="zeroichi_members">
-        <div class="bg">
-            <div class="triangle1"></div>
-        </div>
-        <section id="page_mv">
-            <h2 class="title">01MEMBERS</h2>
-            <h2 class="title_ja">メンバー紹介</h2>
-        </section>
-        <section id="members">
-            <div class="wrapper">
+<main id="zeroichi_members">
+    <div class="bg">
+        <div class="triangle1"></div>
+    </div>
+    <section id="page_mv">
+        <h2 class="title">01MEMBERS</h2>
+        <h2 class="title_ja">メンバー紹介</h2>
+    </section>
+    <section id="members">
+        <div class="wrapper">
                 <div class="member-flex-parent">
                     <div class="member">
                         <div class="member-img">
@@ -18,16 +18,16 @@
                         </div>
                         <div class="member-content">
                             <h2>奥田悠友</h2>
-                            <ul  class="u_paragraph_min">
+                            <ul class="u_paragraph_min">
                                 <li>営業</li>
                             </ul>
                             <div class="member_sns_wrapper">
-                                <ul class="member_sns">
+                                <!-- <ul class="member_sns">
                                     <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
                                     <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
                                     <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
                                     <li><a href="https://lin.ee/" class="flowbtn17 fl_li2"><i class="fab fa-line fa-3x"></i></a></li>
-                                </ul>
+                                </ul> -->
                             </div>
                         </div>
                     </div>
@@ -43,10 +43,10 @@
                                 <li>プロジェクトマネージャー</li>
                             </ul>
                             <div class="member_sns_wrapper">
-                                <ul class="member_sns">
+                                <!-- <ul class="member_sns">
                                     <li><a href="https://twitter.com/" class="flowbtn17 fl_tw2"><i class="fab fa-twitter fa-3x"></i></a></li>
                                     <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
-                                </ul>
+                                </ul> -->
                             </div>
                         </div>
                     </div>
@@ -77,18 +77,18 @@
                                 <li>映像クリエイター</li>
                             </ul>
                             <div class="member_sns_wrapper">
-                                <ul class="member_sns">
+                                <!-- <ul class="member_sns">
 
                                     <li><a href="https://www.instagram.com/kenshiro7110/" class="flowbtn17 insta_btn2"><i class="fab fa-instagram fa-3x"></i></a></li>
                                     <li><a href="https://www.youtube.com/user/" class="flowbtn17 fl_yu2"><i class="fab fa-youtube fa-3x"></i></a></li>
 
-                                </ul>
+                                </ul> -->
                             </div>
                         </div>
                     </div>
                     <div class="member">
                         <div class="member-img">
-                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon.jpg")) ?>" alt="img">
+                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon-3.jpg")) ?>" alt="img">
                         </div>
                         <div class="member-content">
                             <h2>内藤健士郎</h2>
@@ -103,7 +103,7 @@
                     </div>
                     <div class="member">
                         <div class="member-img">
-                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon.jpg")) ?>" alt="img">
+                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon-3.jpg")) ?>" alt="img">
                         </div>
                         <div class="member-content">
                             <h2>庄司歩夢</h2>
@@ -118,7 +118,7 @@
                     </div>
                     <div class="member">
                         <div class="member-img">
-                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon.jpg")) ?>" alt="img">
+                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon-3.jpg")) ?>" alt="img">
                         </div>
                         <div class="member-content">
                             <h2>小筆赳 </h2>
@@ -133,7 +133,7 @@
                     </div>
                     <div class="member">
                         <div class="member-img">
-                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon.jpg")) ?>" alt="img">
+                            <img src="<?php echo esc_url(get_theme_file_uri("/img/coming-soon-3.jpg")) ?>" alt="img">
                         </div>
                         <div class="member-content">
                             <h2>曽我隼人</h2>
@@ -146,11 +146,9 @@
                             </div>
                         </div>
                     </div>
-
-                            
-                </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-    </main>
-    <?php get_footer(); ?>
+</main>
+<?php get_footer(); ?>
